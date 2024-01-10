5e8162a37ae90fc0c09b1f594a5f37164f92bbd3
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2024-01-10 16:31:14 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+import Splide from '@splidejs/splide';
+
+// デフォルトのテーマ
+import '@splidejs/splide/css';
+
+// または、そのほかのテーマ
+import '@splidejs/splide/css/skyblue';
+import '@splidejs/splide/css/sea-green';
+
+// あるいは、コアスタイルのみ
+import '@splidejs/splide/css/core';
+
+new Splide( '.splide' ).mount();
